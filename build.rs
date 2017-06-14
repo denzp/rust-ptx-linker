e7e8a67a957cd713a02e5b0c1337185a37eebbbb
Denys <denys.zariaiev@gmail.com>
2017-06-14 09:00:01 +0200

    Travis: build with both GCC and Clang

--- build.rs
+++ build.rs
@@ -8,6 +8,7 @@ fn main() {
         .flag("-pedantic")
         .flag("-Wall")
         .flag("-Werror")
+        .flag("-std=c++11")
         .flag("-D_GLIBCXX_USE_CXX11_ABI=0")
         .file("llvm/external-refs.cpp")
         .compile("librust-ptx-llvm-stuff.a");
